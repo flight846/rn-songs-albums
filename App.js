2dbcfb642b78720978a99ed556f75cd561bee889
yazid <yazid@decision-science.com>
2019-03-25 23:25:25 +0800

    Add header

---
+++ App.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import Header from './src/components/Header';
+
+class App extends Component {
+    render() {
+        return (
+            <Header headerText={ 'Albums' }/>
+        );
+    }
+}
+
+export default App;
